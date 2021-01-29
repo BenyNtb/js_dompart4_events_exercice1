a74458ee11e2c903e61fc13a68728e67475544d6
Beny <b.ntambwe97@gmail.com>
2021-01-29 09:49:14 +0100

    +correction

--- main.js
+++ main.js
@@ -23,3 +23,27 @@ let bonsoir = () => {
     console.log('Ouai Bonsoir');
 }
 exo1.addEventListener('click', bonsoir)
+
+
+
+
+
+
+// CORRECTION 
+
+// Envents Exercice1
+
+//1
+// méthode1
+let myH1 = document.querySelector('h1')
+
+// let colorH1 = () => {
+//     myH1.style.color = 'blue'
+// } 
+// myH1.addEventListener('click', colorH1)
+
+// methode2
+// myH1.addEventListener('click', () => {
+//     myH1.style.color = 'blue'
+// })
+4
